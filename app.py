2b243065b868416e63192b43afde7f79c792e92b
Zimin Yue <z.yue1220231@arts.ac.uk>
2025-02-22 23:31:51 +0000

    Update app.py
    Make the camera window full-screen, but gesture window show nothing at all

--- app.py
+++ app.py
@@ -4,19 +4,52 @@ import math
 import statistics
 import net_init
 import numpy as np
-import pyautogui
+#import pyautogui
 
-# 获取屏幕宽度和高度
-screen_width, screen_height = pyautogui.size()
+# # 获取屏幕宽度和高度
+# screen_width, screen_height = pyautogui.size()
 
-# 设置每个窗口的宽度为屏幕的一半，保持高度不变
-camera_width = screen_width // 2
-camera_height = screen_height
+# # 设置每个窗口的宽度为屏幕的一半，保持高度不变
+# camera_width = screen_width // 2
+# camera_height = screen_height
 
-gesture_width = screen_width // 2
-gesture_height = screen_height
+# gesture_width = screen_width // 2
+# gesture_height = screen_height
 
 video_capture = cv2.VideoCapture(0)
+
+if not video_capture.isOpened():
+    print("Cannot access the camera.")
+    exit()
+else:
+    print("Camera OK.")
+
+# 获取视频的宽度和高度
+# frame_width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+# frame_height = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+# # 设置目标区域的宽度和高度（假设你想裁剪为中心区域）
+# target_width = 640
+# target_height = 480
+
+# # 设置文字参数
+# font = cv2.FONT_HERSHEY_SIMPLEX
+# font_scale = 1
+# color = (255, 255, 255)  # 白色
+# thickness = 2
+
+# while True:
+#     # 读取摄像头的帧
+#     ret, frame = video_capture.read()
+#     if not ret:
+#         print("Failed to capture image.")
+#         break
+
+#     # 裁剪中心区域
+#     x_start = (frame_width - target_width) // 2
+#     y_start = (frame_height - target_height) // 2
+#     cropped_frame = frame[y_start:y_start+target_height, x_start:x_start+target_width]
+
 
 mediapipe = mp.solutions.hands
 mediapipe_hands = mediapipe.Hands(static_image_mode = False, max_num_hands = 1, min_detection_confidence = 0.5, min_tracking_confidence = 0.5)
@@ -189,12 +222,23 @@ while video_capture:
             forward_inverse_threshold = cv2.threshold(foward_threshold, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1] 
             forward_inverse_threshold = cv2.flip(forward_inverse_threshold, 1)
 
-            #Resize gesture window
-            gesture_frame_resized = cv2.resize(test_frame, (gesture_width, gesture_height))
-            # 显示手势识别窗口，设置为屏幕左边
-            cv2.imshow('Gesture', gesture_frame_resized)
-            cv2.moveWindow('Gesture', 0, 0)  # 左边
-            # cv2.imshow('Hand Segmentation', forward_inverse_threshold)  
+            
+            # # 在手势窗口中显示黑色背景
+            # gesture_window = np.zeros((frame_height, frame_width, 3), dtype=np.uint8)
+
+            #  # 在手势窗口中插入裁剪后的摄像头画面
+            # gesture_window[y_start:y_start+target_height, x_start:x_start+target_width] = cropped_frame
+            # print("Displaying gesture window...")
+            # cv2.imshow('Hand Segmentation', forward_inverse_threshold)
+        
+            # #Resize gesture window
+            # # gesture_frame_resized = cv2.resize(test_frame, (gesture_width, gesture_height))
+            # # # 显示手势识别窗口，设置为屏幕左边
+            # # cv2.imshow('Gesture', gesture_frame_resized)
+            # # cv2.moveWindow('Gesture', 0, 0)  # 左边
+            cv2.imshow('Hand Segmentation', forward_inverse_threshold)  
+            
+        
 
         if centroid_calculation_iteration == 1:
             centroid_position_xy_1.append(((hand_bounding_box_x_min + hand_bounding_box_x_max) / 2))
@@ -205,7 +249,7 @@ while video_capture:
             centroid_position_xy_2.append(((hand_bounding_box_y_min + hand_bounding_box_y_max) / 2))   
 
             hand_centroid_velocity = float(calculate_hand_centroid_velocity(centroid_position_xy_1, centroid_position_xy_2))
-
+            
             if net_testing == False and hand_in_frame == True and hand_centroid_velocity != 0 and hand_centroid_velocity < 1.5 and hand_position_eval == 'IN_RANGE' and test_frame_x1 >= 0 and test_frame_x2 <= frame_x_max and test_frame_y1 >= 0 and test_frame_y2 <= frame_y_max:             
                 net_testing = True
 
@@ -220,7 +264,7 @@ while video_capture:
                 foward_threshold = cv2.threshold(forward_frame_mask, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1] 
                 forward_inverse_threshold = cv2.threshold(foward_threshold, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1] 
                 forward_inverse_threshold_resized = cv2.resize(forward_inverse_threshold, dsize=(28, 28), interpolation = cv2.INTER_AREA)  
-
+        
                 net_softmax_output = net_init.trained_forward_propagation(forward_inverse_threshold_resized)
                 net_softmax_output_list = net_softmax_output.tolist()
 
@@ -249,6 +293,7 @@ while video_capture:
       
     analytics_frame = cv2.flip(analytics_frame, 1)
 
+
     cv2.putText(analytics_frame, "Hit Q to QUIT!", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 98, 255), 2, cv2.LINE_AA)
     cv2.putText(analytics_frame, "hand_in_frame: {}".format(str(hand_in_frame)), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2, cv2.LINE_AA)
     cv2.putText(analytics_frame, "hand_position_eval: {}".format(hand_position_eval), (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2, cv2.LINE_AA)
@@ -259,17 +304,48 @@ while video_capture:
     cv2.putText(analytics_frame, "net_output_translation_confidence: {}".format(net_output_translation_confidence), (10, 230), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2, cv2.LINE_AA)
     cv2.putText(analytics_frame, "{}".format(net_output_translation), (10, 600), cv2.FONT_HERSHEY_SIMPLEX, 10, (0, 0, 255), 5, cv2.LINE_AA)
 
-    #resize camera window
-    camera_frame_resized = cv2.resize(original_frame, (camera_width, camera_height))
-    # 显示摄像头画面窗口，设置为屏幕右边
-    cv2.imshow('Camera', camera_frame_resized)
-    cv2.moveWindow('Camera', camera_width, 0)  # 右边
+    # print("Displaying cropped frame...")
+    # # 显示摄像头窗口
+    # cv2.imshow('How you are doing', cropped_frame)
+
+    # # resize camera window
+    # # camera_frame_resized = cv2.resize(original_frame, (camera_width, camera_height))
+    # # # 显示摄像头画面窗口，设置为屏幕右边
+    # cv2.imshow('How you are doing', camera_frame_resized)
+    # cv2.moveWindow('How you are doing', camera_width, 0)  # 右边
+
+    # cv2.namedWindow("Analytics", cv2.WND_PROP_FULLSCREEN)
+    # cv2.setWindowProperty("Analytics", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+   
+    # 获取屏幕分辨率
+    screen_width = 1920  # 屏幕宽度
+    screen_height = 1080  # 屏幕高度
 
 
-    # cv2.imshow("Analytics", analytics_frame)
+    # 创建窗口并设置为全屏
+    cv2.namedWindow("Analytics", cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty("Analytics", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
     
+    # 创建一个小窗口来显示 Hand Segmentation
+    cv2.namedWindow("Hand Segmentation", cv2.WND_PROP_FULLSCREEN)
+    # 可以设置窗口的位置来确保它显示在 Analytics 窗口之上
+    cv2.moveWindow("Hand Segmentation", 100, 100)  # 调整这里的位置
+
+    # 假设你有一个全屏的 `analytics_frame` 和一个小的 `hand_segmentation_frame`
+    # analytics_frame = np.zeros((screen_height, screen_width, 3), dtype=np.uint8)
+    hand_segmentation_frame = np.zeros((200, 300, 3), dtype=np.uint8)  # 小的手部分割图像
+
+    # 在 Analytics 窗口中显示更新后的图像
+    cv2.imshow("Analytics", analytics_frame)
+
+    # 在 Hand Segmentation 窗口中显示更新后的图像
+    cv2.imshow("Hand Segmentation", hand_segmentation_frame)
+
+        
+        
     if cv2.waitKey(25) == ord('q'):
-        break
+            break
 
 video_capture.release()
 cv2.destroyAllWindows()
