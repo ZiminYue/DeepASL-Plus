bf8174098cedf679747e3062203d10b442b61e4d
Zimin Yue <z.yue1220231@arts.ac.uk>
2025-02-25 12:17:25 +0000

    Create gettyHands.py

---
+++ DeepASLWorking/MyGestureModel/gettyHands.py
@@ -0,0 +1,113 @@
+#Source: https://datadrivenscience.com/hand-gesture-recognition-with-pytorch-a-comprehensive-tutorial/
+# Import necessary packages
+import os
+import numpy as np
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+# Load the data
+class LeapGestRecog(Dataset):
+    def __init__(self, root, transform=None):
+        self.root = root
+        self.transform = transform
+        self.x = []
+        self.y = []
+
+        folders = os.listdir(root)
+        for folder in folders:
+            for dirpath, dirnames, filenames in os.walk(os.path.join(root, folder)):
+                for filename in filenames:
+                    self.x.append(os.path.join(dirpath, filename))
+                    self.y.append(folder)
+        self.len = len(self.x)
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, index):
+        img = Image.open(self.x[index]).convert('L')
+        y = self.y[index]
+        if self.transform:
+            img = self.transform(img)
+        return img, y
+
+root = './data/leapGestRecog/'
+transform = transforms.Compose([transforms.Resize((128, 128)), transforms.ToTensor()])
+dataset = LeapGestRecog(root, transform=transform)
+
+#Splitting data into train and test set
+train_size = int(0.8 * len(dataset))
+test_size = len(dataset) - train_size
+train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+
+# Building the Model
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=5)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=5)
+        self.fc1 = nn.Linear(128*12*12, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv3(x))
+        x = x.view(x.size(0), -1)  # Flatten layer
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+model = Net()
+
+# Training the Model
+
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=64, shuffle=True)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.to(device)
+
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+loss_fn = nn.CrossEntropyLoss()
+
+def train(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = loss_fn(output, target)
+        loss.backward()
+        optimizer.step()
+        print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            epoch, batch_idx * len(data), len(train_loader.dataset),
+            100. * batch_idx / len(train_loader), loss.item()))
+
+for epoch in range(1, 11):
+    train(epoch)
+
+# Testing the Model
+def test():
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += loss_fn(output, target).item()
+            pred = output.data.max(1, keepdim=True)[1]
+            correct += pred.eq(target.data.view_as(pred)).sum()
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+test()
