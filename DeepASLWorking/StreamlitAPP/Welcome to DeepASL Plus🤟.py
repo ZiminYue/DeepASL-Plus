9ce6f98c17b0fcd883f090836d6a3f6841bc2e95
Zimin Yue <z.yue1220231@arts.ac.uk>
2025-03-02 21:11:14 +0000

    Update Welcome to DeepASL Plus🤟.py

--- DeepASLWorking/StreamlitAPP/Welcome to DeepASL Plus🤟.py
+++ DeepASLWorking/StreamlitAPP/Welcome to DeepASL Plus🤟.py
@@ -1,1 +1,9 @@
-#do I have to add something?
+import streamlit as st
+
+# [theme]
+base="dark"
+primaryColor="purple"
+font="serif"
+
+st.title("DeepASL Plus")
+st.subheader("Try out American Sign Language in real time!")
